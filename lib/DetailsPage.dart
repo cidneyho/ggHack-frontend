c009be0fbb66a1b5656586d6b741031f8f7ec1e1
cyd <yhoad@connect.ust.hk>
2020-06-15 01:17:48 +0800

    implemented timetable

--- lib/DetailsPage.dart
+++ lib/DetailsPage.dart
@@ -71,8 +71,86 @@ class DetailsPage extends StatelessWidget {
                 ),
               )
           ),
+          // time table's title
+          GestureDetector(
+              onTap: () {},
+              child: new Container(
+                padding: const EdgeInsets.only(left: 20.0, top: 20.0, right: 20.0, bottom: 14.0),
+                child: new Row(
+                  children: [
+                    new Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        new Container(
+                            child: new Text(
+                                "Available slots",
+                                style: new TextStyle(fontWeight: FontWeight.bold,))
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              )
+          ),
+          // time table
+          _renderTable(_freeSlots),
         ]
       )
     );
+  }
+
+  var _freeSlots = [
+    [0,0,0,0,0,0,0,0,1,1,2,2,3,3,4,4,2,2,1,1,0,0,0,0], // Mon
+    [0,0,0,0,0,0,0,0,1,1,3,3,2,2,1,1,1,1,1,1,0,0,0,0], // Tue
+    [0,0,0,0,0,0,0,0,1,1,1,1,2,2,2,2,3,3,2,2,1,1,0,0],
+    [0,0,0,0,0,0,0,0,1,1,1,1,2,2,2,2,1,1,1,1,0,0,0,0],
+    [0,0,0,0,0,0,0,0,1,1,1,1,2,2,2,2,1,1,1,1,1,1,0,0],
+    [0,0,0,0,0,0,0,0,1,1,2,2,3,3,3,2,1,1,1,1,1,1,0,0],
+    [0,0,0,0,0,0,0,0,1,1,2,2,3,3,2,2,1,1,1,1,1,1,0,0],
+  ];
+
+  Widget _renderTable(List<List<int>> data) {
+    var days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+    return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: Table(
+          border: TableBorder.all(color: Colors.black26, width: 1, style: BorderStyle.solid),
+          children: [
+            TableRow(children: [
+              for (var day in days) TableCell(child: Container(
+                padding: const EdgeInsets.all(2.0),
+                child: Center(child: Text(day))))
+            ]),
+            for (int t=0; t<24; t++)
+              TableRow(children: [
+                for (int d=0; d<7; d++) TableCell(child: Container(
+                  color: _gradient(data[d][t]),
+                  padding: const EdgeInsets.all(3.0),
+                  child: Center(child: Text(data[d][t].toString()))))
+              ])
+          ]
+        ));
+  }
+
+  Color _gradient(int data) {
+    Color color;
+    switch(data) {
+      case 0:
+        color = Colors.transparent;
+        break;
+      case 1:
+        color = colorTone1;
+        break;
+      case 2:
+        color = colorTone2;
+        break;
+      case 3:
+        color = colorTone3;
+        break;
+      default:
+        color = colorTone4;
+        break;
+    }
+    return color;
   }
 }
--- lib/helpers/Constants.dart
+++ lib/helpers/Constants.dart
@@ -5,6 +5,11 @@ Color colorDark = Color.fromRGBO(58, 66, 86, 1.0);
 Color colorBase = Color.fromRGBO(64, 75, 96, 0.9);
 Color colorLoom = Colors.white54;
 
+Color colorTone1 = Colors.blue[100];
+Color colorTone2 = Colors.blue[300];
+Color colorTone3 = Colors.blue[700];
+Color colorTone4 = Colors.blue[900];
+
 // Pages
 const loginPageTag = 'Login Page';
 const homePageTag = 'Home Page';
